75cc2c6a97a04cb929a464691905662216116d8a
Краєвський Олексій Андрійович <vt241_koa@student.ztu.edu.ua>
2026-02-23 20:16:24 +0200

    added .md about programming principles in project

--- AquaCare/Plant.xaml.cs
+++ AquaCare/Plant.xaml.cs
@@ -55,7 +55,8 @@ namespace AquaCare
                     if (!string.IsNullOrEmpty(species.ImagePath))
                     {
                         string potentialPath = System.IO.Path.Combine(baseDir, species.ImagePath);
-                        if (System.IO.File.Exists(potentialPath)) finalPath = potentialPath;
+                        if (System.IO.File.Exists(potentialPath)) 
+                            finalPath = potentialPath;
                     }
 
                     plantCardsList.Add(new PlantCardData
